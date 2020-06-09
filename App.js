aae1753c96f2049e5415ddaa659ac22d36316c81
akshaypatil581 <akshaypatil581@gmail.com>
2020-06-08 19:47:52 -0700

    Added the delete item functionality

--- App.js
+++ App.js
@@ -1,14 +1,35 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import Header from './components/Header';
+import ListItem from './components/ListItem';
+import {uuid} from 'uuidv4';
 
 const App = () => {
+  
+
+  const [items, setItems] = useState([
+    {id: Math.random(), text: 'Milk' },
+    {id: Math.random(), text: 'Eggs' },
+    {id: Math.random(), text: 'Bread' },
+    {id: Math.random(), text: 'Juice' },
+  ]);
+
+  const deleteItem = (id) => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id != id);
+    });
+  }
+
   return (
     <View style={styles.container}>
       <Header />
+      <FlatList 
+        data={items} 
+        renderItem={({item}) => <ListItem item={item} deleteItem={deleteItem}/>} 
+      />
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
